a815998fc6f7d3c191a9d911fcc3641abf7c099b
dustyposa <zmzposa@outlook.com>
2020-03-10 23:10:12 +0800

    :memo: add send_email.py

--- small_projects/邮件发送/email_gui.py
+++ small_projects/邮件发送/email_gui.py
@@ -1,4 +1,8 @@
+from typing import Any, Dict
+
 import PySimpleGUI as sg
+
+from send_email import send_emails
 
 sg.theme('BluePurple')
 
@@ -9,18 +13,40 @@ layout = [
     [sg.Text('请输入用户名和密码:')],
     [sg.Text('用户名:', size=(14, 1)), sg.Input(key=user_input_key)],
     [sg.Text('密码:', size=(14, 1)), sg.Input(key=pwd_input_key, password_char="*")],
-    [sg.Text('发送的邮箱文件:', size=(14, 1)), sg.FileBrowse("浏览", size=(8, 1))],
+    [sg.Text('发送的内容:', size=(14, 3), justification="center"), sg.Input(size=(14, 3), key="contents")],
+
+    [sg.Text('发送的邮箱文件:', size=(14, 1)), sg.FileBrowse("浏览", size=(8, 1), key="files")],
     [sg.ProgressBar(500, orientation='h', size=(80, 20), key='progbar', style='winnative', relief='52%')],
-    [sg.Button('确认'), sg.Button('退出', key='Exit')],
+    [sg.Button('确认', key='submit'), sg.Button('退出', key='Exit')],
 ]
 window = sg.Window('邮件发送', layout)
+
+
+def check_data(values: Dict[str, Any]) -> bool:
+    for key in [user_input_key, pwd_input_key, "files", "contents"]:
+        if not values.get(key):
+            return False
+    return True
+
+
 while True:
     event, values = window.read()
     user, pwd = values[user_input_key], values[pwd_input_key]
     print(event, values)
     if event in (None, 'Exit'):
         break
-    window['progbar'].update_bar(200 + 1)
+    elif event == "submit":
+        if check_data(values):
+            send_emails(
+                user=values[user_input_key],
+                pwd=values[pwd_input_key],
+                contents=[
+                    values["contents"],
+                    values["files"],
+                ]
+            )
+    # for i in range(500):
+    #     window['progbar'].update_bar(i + 1)
     # if event == 'Show':
     # Update the "output" text element to be the value of "input" element
     # window['-OUTPUT-'].update(values['-IN-'])
--- small_projects/邮件发送/send_email.py
+++ small_projects/邮件发送/send_email.py
@@ -1,20 +1,34 @@
-import os
+from typing import List
 
 import yagmail
-from dotenv import load_dotenv
 
-load_dotenv()
-# 链接邮箱服务器
-yag = yagmail.SMTP(user=os.getenv("USER_NAME"), password=os.getenv("USER_PWD"), host='smtp.163.com')
 
-# 邮箱正文
-contents = [
-    '这是一封正常邮件，需要测试',
-    'You can find an audio file attached.',
-    # yagmail.inline("./Snipaste_2020-03-09_23-25-23.png"),
-    "./Snipaste_2020-03-09_23-25-23.png",
-]
+def send_emails(
+        user: str,
+        pwd: str,
+        contents: List[str],
+        send_list: List[str]
+) -> None:
+    yag = yagmail.SMTP(user=user, password=pwd, host='smtp.163.com')
+    yag.send(send_list, contents)
 
-# 发送邮件
-yag.send(os.getenv("SEND_EMAIL"), '重要消息cc', contents)
-print("发送成功")
+
+if __name__ == '__main__':
+    import os
+    from dotenv import load_dotenv
+
+    load_dotenv()
+    # 链接邮箱服务器
+    yag = yagmail.SMTP(user=os.getenv("USER_NAME"), password=os.getenv("USER_PWD"), host='smtp.163.com')
+
+    # 邮箱正文
+    contents = [
+        '这是一封正常邮件，需要测试',
+        'You can find an audio file attached.',
+        # yagmail.inline("./Snipaste_2020-03-09_23-25-23.png"),
+        "./Snipaste_2020-03-09_23-25-23.png",
+    ]
+
+    # 发送邮件
+    yag.send(os.getenv("SEND_EMAIL"), '重要消息cc', contents)
+    print("发送成功")
