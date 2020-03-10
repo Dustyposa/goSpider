f22491873d1c8c935951ac4d817b0e279f50db86
zmzposa <zmzposa@outlook.com>
2020-03-10 18:18:50 +0800

    :memo: update 邮件发送小工具 add processbar

--- small_projects/邮件发送/email_gui.py
+++ small_projects/邮件发送/email_gui.py
@@ -5,11 +5,14 @@ sg.theme('BluePurple')
 user_input_key = '-User-'
 pwd_input_key = '-PWD-'
 
-layout = [[sg.Text('请输入用户名和密码:'), sg.Text(size=(15, 1), key='-OUTPUT-')],
-          [sg.Text('用户名:', size=(14, 1)), sg.Input(key=user_input_key)],
-          [sg.Text('密码:', size=(14, 1)), sg.Input(key=pwd_input_key)],
-          [sg.Text('发送的邮箱文件:', size=(14, 1)), sg.FileBrowse("浏览", size=(8, 1))],
-          [sg.Button('确认'), sg.Button('退出')]]
+layout = [
+    [sg.Text('请输入用户名和密码:')],
+    [sg.Text('用户名:', size=(14, 1)), sg.Input(key=user_input_key)],
+    [sg.Text('密码:', size=(14, 1)), sg.Input(key=pwd_input_key, password_char="*")],
+    [sg.Text('发送的邮箱文件:', size=(14, 1)), sg.FileBrowse("浏览", size=(8, 1))],
+    [sg.ProgressBar(500, orientation='h', size=(80, 20), key='progbar', style='winnative', relief='52%')],
+    [sg.Button('确认'), sg.Button('退出', key='Exit')],
+]
 window = sg.Window('邮件发送', layout)
 while True:
     event, values = window.read()
@@ -17,9 +20,10 @@ while True:
     print(event, values)
     if event in (None, 'Exit'):
         break
+    window['progbar'].update_bar(200 + 1)
     # if event == 'Show':
-        # Update the "output" text element to be the value of "input" element
-        # window['-OUTPUT-'].update(values['-IN-'])
+    # Update the "output" text element to be the value of "input" element
+    # window['-OUTPUT-'].update(values['-IN-'])
     print(user, pwd)
 
 window.close()
