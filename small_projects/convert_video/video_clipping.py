d7d03149b5573a50fe210c1da05c91a7a34b3778
zmzposa <zmzposa@outlook.com>
2020-03-24 18:10:47 +0800

    :memo: 迭代器，生成器，协程

--- small_projects/convert_video/video_clipping.py
+++ small_projects/convert_video/video_clipping.py
@@ -19,7 +19,7 @@ outputs_dir_name = "outputs"
 
 
 def main() -> None:
-    Path(mp4_path / outputs_dir_name).mkdir(exist_ok=True)  # 创建输出文件夹
+    (Path(mp4_path) / outputs_dir_name).mkdir(exist_ok=True)  # 创建输出文件夹
     for file in Path(mp4_path).glob("*.mp4"):
         cut_video(file.absolute().__str__())
 
@@ -28,15 +28,20 @@ def cut_video(file_name: str):
     video_path = file_name
     tmp = Path(file_name)
     output_path = (tmp.parent / outputs_dir_name).joinpath(tmp.stem + "{}").with_suffix(".mp4").absolute().__str__()
-    cut_time = 12  # unit s
+    cut_time = 5  # unit s
     total_duration = get_length(file_name) - 20
     splice_time = 10 * 60  # 分割时间
     index = 1
     while (total_duration - cut_time) / splice_time > 2:
-        ffmpeg_extract_subclip(video_path.format(""), cut_time, cut_time + splice_time,
-                               targetname=output_path.format(index))
+        ffmpeg_extract_subclip(
+            video_path.format(""),
+            cut_time,
+            cut_time + splice_time,
+            targetname=output_path.format(index)
+        )
         cut_time += splice_time
         index += 1
+        print(f"{output_path.format(index)}文件生成成功。")
     ffmpeg_extract_subclip(video_path.format(""), cut_time, total_duration, targetname=output_path.format(index))
     print(f"{output_path.format(index)}文件生成成功。")
 
