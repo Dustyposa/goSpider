b11c6213d5c04101590c19eb23ab9529129a910e
dustyposa <zmzposa@outlook.com>
2020-05-21 18:08:27 +0800

    :docs: update dzdp comments_dzdp.py

--- spider_project/dzdp/comments/comments_dzdp.py
+++ spider_project/dzdp/comments/comments_dzdp.py
@@ -37,11 +37,13 @@ class CommentsCrawler:
     def __init__(self, shop_id: str) -> None:
         self.shop_id = shop_id
         self.base_url = f"http://www.dianping.com/shop/{self.shop_id}/review_all/p"
-        self.now_page = 5
+        self.now_page = 8
         self.crawl_url = self.base_url + str(self.now_page)
         self.headers = COMMENTS_HEADERS
         self.db_client = DBClient("dzdp")
         self.session = requests.Session()
+        self.session.headers.update(self.headers)
+        self.now_content = b""
 
     def increase_page(self) -> None:
         self.now_page += 1
@@ -61,15 +63,22 @@ class CommentsCrawler:
                 self.increase_page()
             else:
                 print("没有数据了。")
-                with open("crawled_db", "a") as fp:
-                    fp.write(self.shop_id + "\n")
+                self.record_id()
                 break
-            time.sleep(5)
+            time.sleep(15)
+
+    def record_id(self) -> None:
+        Path("fail_data").write_bytes(self.now_content)
+        with open("crawled_db", "a") as fp:
+            fp.write(f"{self.shop_id},{self.now_page}\n")
 
     def crawl_page(self) -> CommentsParser:
-        response = self.session.get(self.crawl_url, headers=self.headers)
+        if self.now_page > 1:
+            self.session.headers["Referer"] = self.base_url + str(self.now_page - 1)
+        # print(self.session.headers)
+        response = self.session.get(self.crawl_url)
         response.raise_for_status()
-
+        self.now_content = response.content
         text = response.text
         parser = CommentsParser(html=text)
         return parser
