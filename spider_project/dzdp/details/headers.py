4756cc4516088e72607bb4ccdc5c49e5479c11f8
dustyposa <zmzposa@outlook.com>
2020-06-02 16:50:52 +0800

    :star: add dzdp detail headers

--- spider_project/dzdp/details/headers.py
+++ spider_project/dzdp/details/headers.py
@@ -1,3 +1,5 @@
+import dataclasses
+
 COMMON_HEADERS = {
     "Host": "www.dianping.com",
     "User-Agent": "Mozilla/5.0 (Windows NT 6.1; Win64; x64; rv:74.0) Gecko/20100101 Firefox/74.0",
@@ -5,8 +7,15 @@ COMMON_HEADERS = {
     "Accept-Language": "zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2",
     "Accept-Encoding": "gzip, deflate, br",
     "Connection": "keep-alive",
-    "Cookie": "cy=8; cityid=8; cye=chengdu; fspop=test; cy=8; cye=chengdu; Hm_lvt_602b80cf8079ae6591966cc70a3940e7=1590478732; Hm_lpvt_602b80cf8079ae6591966cc70a3940e7=1590478781; _lx_utm=utm_source%3DBaidu%26utm_medium%3Dorganic; _lxsdk_cuid=1724feb7cf029-094a636525b318-153b7540-1fa400-1724feb7cf1c8; _lxsdk_s=1724feb7cf1-05d-1cb-e0b%7C%7C124; _lxsdk=1724feb7cf029-094a636525b318-153b7540-1fa400-1724feb7cf1c8; _hc.v=941ddd77-cad4-3c88-422b-3a86841a1166.1590478733",
+    "Cookie": "",
     "Upgrade-Insecure-Requests": "1",
     "Pragma": "no-cache",
     "Cache-Control": "no-cache",
 }
+
+ALL_REVIEW_PARAMS = {'shopId': 'H1aHdhkb51pd6oXh',
+                     '_token': '',
+                     'originUrl': 'http://www.dianping.com/shop/H1aHdhkb51pd6oXh',
+                     'shopType': 10,
+                     'cityId': 8}
+
